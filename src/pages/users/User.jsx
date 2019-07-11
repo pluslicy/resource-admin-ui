50bd1bd2d04f8064d209a6e0afa4c4dd9a72bb0c
otatoz <279923215@qq.com>
2019-07-11 09:47:20 +0800

    许品锋：用户管理模块

--- src/pages/users/User.jsx
+++ src/pages/users/User.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-
+import {Button,Input,Table} from 'antd';
+import style from './User.less'
 class User extends React.Component {
  
   render(){
     return (
-      <div>
-        <p>用户管理</p>
+      <div className={style.Back}>
+        <div>
+          <Button>添加</Button>
+          <Input style={{width:'20%'}} />
+        </div>
+        <Table />
       </div>
     )
   }
 }
+
 
 export default User;
