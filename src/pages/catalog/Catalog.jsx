d37b58367714749e87a96381eeed760f83f606af
LiJingCute <1075456031@qq.com>
2019-07-30 16:19:59 +0800

    李静：完善编目管理的页面布局

--- src/pages/catalog/Catalog.jsx
+++ src/pages/catalog/Catalog.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styles from './catelog.less'
 
 class Catalog extends React.Component {
+	
 	render(){
 		return (
-			<div>编目管理</div>
+			
+		<div className={styles.content}>
+			<div className={styles.left}></div>
+
+			<div  className={styles.right}>
+				<div  className={styles.middle}>操作面板 ｜ 视频库编目</div>
+
+			</div>
+				
+		</div>
 			)
 	}
 }
