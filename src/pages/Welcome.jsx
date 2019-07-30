73bf45017820886ac90c01f2543e78aef1842e1d
AdminRDC <chen_2019@qq.com>
2019-07-30 21:44:16 +0800

    任殿臣:新增首页代码

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import styles from './Welcome.less';
 export default () => (
-  <p
-    style={{
-      textAlign: 'center',
-    }}
-  >
-    Want to add more pages? Please refer to{' '}
-    <a href="https://pro.ant.design/docs/block-cn" target="_blank" rel="noopener noreferrer">
-      use block
-    </a>
-    。
-  </p>
+  <div>
+    <div className={styles.content} style={{ backgroundColor: 'rgba(251, 173, 76, 1)' }} >
+      <div className={styles.imgs}><img src={require('../../images/index/u50.png')} /></div>
+      <div className={styles.name}><span>用户</span><br /><span>325</span></div>
+    </div>
+    <div className={styles.content} style={{ backgroundColor: 'rgba(89, 208, 93, 1)' }}>
+      <div className={styles.imgs}><img src={require('../../images/index/u54.png')} /></div>
+      <div className={styles.name}><span>视频</span><br /><span>2357</span></div>
+    </div>
+    <div className={styles.content} style={{ backgroundColor: 'rgba(255,100,109, 1)' }}>
+      <div className={styles.imgs}><img src={require('../../images/index/u58.png')} /></div>
+      <div className={styles.name}><span>文档</span><br /><span>2435</span></div>
+    </div>
+    <div className={styles.content} style={{ backgroundColor: 'rgba(29,98,240,1)' }}>
+      <div className={styles.imgs}><img src={require('../../images/index/u62.png')} /></div>
+      <div className={styles.name}><span>学校</span><br /><span>352</span></div>
+    </div>
+  </div >
 );
